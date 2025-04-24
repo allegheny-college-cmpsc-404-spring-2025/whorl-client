ec3c92d48f06cac68de763b84219981b676db70b
Aidan Dyga <aidan.dyga@gmail.com>
2025-04-24 14:57:10 -0400

    fix: updating backpack to have example id

--- backpack.py
+++ backpack.py
@@ -4,9 +4,9 @@ from inventory.specs import BackpackSpec
 class Backpack(BackpackSpec):  # Capitalized class name
     """A custom backpack class extending BackpackSpec."""
 
-    def __init__(self, capacity=5):
-        """Initialize the Backpack with a custom capacity."""
-        super().__init__(__name__)
+    def __init__(self, capacity=5, id="example_id_here"):
+        """Initialize the Backpack with a custom capacity and id."""
+        super().__init__(__name__, id=id)  # Pass the id to the parent class
         self.capacity = capacity  # Set the capacity of the backpack
 
     def __str__(self):
